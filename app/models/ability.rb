fde5d46b6d09d184877aa49a2d6e21cf08d8a8e1
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-17 23:55:06 +0100

    Fixed role assignment logic

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.has_role?(:admin)
+    if user.role == "admin"
       can :access, :rails_admin
       can :dashboard
       can :manage, :all
