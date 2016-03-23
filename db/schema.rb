a9198d6cebdd6df95335ee82f8e1faaae274caff
Claudio Brandolino <cbrandolino@gmail.com>
2016-03-23 20:58:35 +0100

    Fixed talent's gender

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322025217) do
+ActiveRecord::Schema.define(version: 20160323195521) do
 
   create_table "allport_contact_cards", force: :cascade do |t|
     t.integer  "contactable_id"
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 20160322025217) do
   create_table "talents", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "gender"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "middle_name"
