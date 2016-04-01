593ec9f771972f84b2ddcfdecda15836cc0bd3d1
Claudio Brandolino <cbrandolino@gmail.com>
2016-04-01 02:10:22 +0200

    Implemented login view layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.new_user_session_path, :alert => exception.message
   end
 
+
+
   def after_sign_in_path_for(resource)
     '/admin'
+  end
+
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
   end
 end
