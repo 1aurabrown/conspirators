58e9a795d7d89d5239267f63ba35cfcc5907c985
Claudio Brandolino <cbrandolino@gmail.com>
2016-04-01 05:01:23 +0200

    Added most_popular_for_context method for taggings

--- app/models/concerns/tag_networkable.rb
+++ app/models/concerns/tag_networkable.rb
@@ -3,12 +3,26 @@ module TagNetworkable
 
   included do 
 
+    def tag_weights(context)
 
-    def context_weight(context)
+    end
+
+    def context_tag_weight(tag_id, tag_ context)
       tag_counts_node = self.tag_counts_on(context)
       most_popular_node = tag_counts_node.maximum(:taggings_count)
-      most_popular_overall = ActsAsTaggableOn::Tag.most_used(1).first.taggings_count
+      most_popular_overall = self.most_popular_for_context(context)
       return most_popular_node / most_popular_overall
+    end
+
+    def context_weight(context)
+
+    end
+
+    def most_popular_for_context(context)
+      ActsAsTaggableOn::Tag.joins(:taggings)
+        .where(["taggings.context = ?", context])
+        .select("DISTINCT tags.*")
+        .most_used(1).first.taggings_count
     end
   end
 end
