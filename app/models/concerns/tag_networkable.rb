f6dde0830045155dc3637b5e61571c6856311299
Claudio Brandolino <cbrandolino@gmail.com>
2016-04-01 05:40:33 +0200

    Added node_context_weight for taggings

--- app/models/concerns/tag_networkable.rb
+++ app/models/concerns/tag_networkable.rb
@@ -7,22 +7,32 @@ module TagNetworkable
 
     end
 
-    def context_tag_weight(tag_id, tag_ context)
+    def context_tag_weight(tag_id, tag_context)
       tag_counts_node = self.tag_counts_on(context)
       most_popular_node = tag_counts_node.maximum(:taggings_count)
       most_popular_overall = self.most_popular_for_context(context)
       return most_popular_node / most_popular_overall
     end
 
-    def context_weight(context)
+    # TagNetworkable-independent
+    def tag_weight(tag_id, context)
 
     end
 
-    def most_popular_for_context(context)
-      ActsAsTaggableOn::Tag.joins(:taggings)
-        .where(["taggings.context = ?", context])
-        .select("DISTINCT tags.*")
-        .most_used(1).first.taggings_count
+
+    # TagNetworkable-dependant
+    def node_context_weight(context)
+      self.tags_on(context).count / self.most_tags_per_node_in(context)
+    end
+
+    def most_tags_per_node_in(context)
+      max_tuple = ActsAsTaggableOn::Tagging
+        .where(taggable_type: "Talent")
+        .where(context: :context)
+        .group(:taggable_id)
+        .count
+        .max_by { | taggable_id, nodes | nodes }
+      return max_tuple[1]
     end
   end
 end
