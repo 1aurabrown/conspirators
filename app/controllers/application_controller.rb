d8d42ff9465af22c56945e080a77f80da12cf2ed
Claudio Brandolino <cbrandolino@gmail.com>
2016-04-08 06:33:57 +0200

    Added impressum to footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   layout :layout_by_resource
+  helper_method :impressum
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.new_user_session_path, :alert => exception.message
   end
 
+  def impressum
+    PageText.find_by(title: 'Impressum').formatted_text
+  end
 
 
   def after_sign_in_path_for(resource)
