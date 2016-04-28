4a4c6a6894bd3554f797bcd64eb7fd7b0aa11bc0
Claudio Brandolino <cbrandolino@gmail.com>
2016-04-28 20:57:07 +0200

    Restyled talent page sections

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -7,7 +7,7 @@ class Talent < ActiveRecord::Base
   has_and_belongs_to_many :projects, inverse_of: :talent
   has_attached_file :avatar, styles: { large: "600>", medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar,  :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  has_attached_file :resume, styles: { thumb: "60x60>" }
+  has_attached_file :resume, styles: { thumb: "60x60>"}
   validates_attachment_content_type :resume, content_type: "application/pdf"
   has_attached_file :cover, styles: { large: "x600" }
   validates_attachment_content_type :cover, content_type: ["image/jpg", "image/jpeg", "image/png" ] 
