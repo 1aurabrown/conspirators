36bf5bce907b9ccd6f24b91a8458460c3416710f
Claudio Brandolino <cbrandolino@gmail.com>
2016-06-03 13:46:13 +0200

    Added inquiries form validations

--- app/assets/javascripts/talents_favourites.coffee
+++ app/assets/javascripts/talents_favourites.coffee
@@ -1,4 +1,9 @@
 app.controllerInitializers.talents_favourites = ->
+  $('form.form input').blur (e) ->
+    if $(e.target).val().length
+      $(e.target).closest('.form-group').addClass('filled')
+    else
+      $(e.target).closest('.form-group').removeClass('filled')
   $('form.form').submit (e) ->
     url = '/api/v1/inquiries'
     # the script where you handle the form input.
--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,4 +1,26 @@
 class InquiriesController < ApplicationController
   def new
+    talent = Talent.find_by slug: params[:id]
+    if current_user.saved_for_later? talent
+      current_user.remove_from_saved talent
+    else
+      current_user.save_for_later talent
+    end
+
+    response = {
+      talent: talent,
+      saved: current_user.saved_for_later?(talent),
+      totalSaved: current_user.saved_talents_num
+    }
+    respond_to do |format|
+      format.json { render json: response }
+    end
+  end
+
+  protected
+
+  def json_request?
+    request.format.json?
+  end
   end
 end
