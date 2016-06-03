78542704583cbb9fb360bd53465b4f178f17908c
Claudio Brandolino <cbrandolino@gmail.com>
2016-06-03 14:03:15 +0200

    Implemented admin side of inquiries

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,11 @@
 class Inquiry < ActiveRecord::Base
+  before_create :set_answered
+  validates_presence_of :name, :email
+  rails_admin do
+    navigation_label 'Admin'
+    configure :answered, :toggle
+  end
+  def set_answered
+    self.answered = false
+  end
 end
