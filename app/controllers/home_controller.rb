69645b93539b6d2bfbfe4259acade280797ccce7
Claudio Brandolino <cbrandolino@gmail.com>
2016-06-11 07:00:45 +0200

    Order homepage talents by popularity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @talents = Talent.recent
-    @tags = Tag.where(acceptable: true)
+    @talents = Talent.order(popularity: :desc).recent
+      @tags = Tag.where(acceptable: true)
     
     gon.tags = Talent.recent.map do |talent|
       {id: talent.id, tags: talent.acceptable_tags.to_ary }
