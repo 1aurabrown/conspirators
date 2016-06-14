eedbec0996d2d9bf8cc085b9b4ecd0f3d8549c7b
Claudio Brandolino <cbrandolino@gmail.com>
2016-06-14 18:27:00 +0200

    Fixed contact text

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,7 +1,7 @@
 class ContactController < ApplicationController
   def index
     @title = "Contact"
-    @description = PageText.description.first.text
+    @description = PageText.contact_general.first.text
     @general = PageText.contact_general.first.formatted_text
     @agencies = PageText.contact_agencies.first.formatted_text
     @talents = PageText.contact_talents.first.formatted_text
